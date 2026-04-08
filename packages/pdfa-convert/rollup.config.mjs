36582824d8815bda4907fe6aa45ed64d786ed18a
Guido Flohr <guido.flohr@cantanea.com>
2026-04-08 16:21:17 +0300

    add project boilerplate

---
+++ packages/pdfa-convert/rollup.config.mjs
@@ -0,0 +1,57 @@
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+
+function tsPlugin(tsconfig) {
+	return typescript({
+		tsconfig,
+		exclude: ["**/*.spec.ts"],
+	});
+}
+
+const external = [
+	// Node built-ins
+	"fs",
+	"path",
+	"stream",
+	"util",
+];
+
+export default [
+	{
+		input: "src/index.ts",
+		output: {
+			file: "dist/index.esm.js",
+			format: "esm",
+			sourcemap: true,
+		},
+		external,
+		plugins: [tsPlugin("./tsconfig.build.json"), nodeResolve(), commonjs()],
+	},
+	{
+		input: "src/index.ts",
+		output: {
+			file: "dist/index.cjs.js",
+			format: "cjs",
+			sourcemap: true,
+			exports: "named",
+		},
+		external,
+		plugins: [tsPlugin("./tsconfig.build.json"), nodeResolve(), commonjs()],
+	},
+	{
+		input: "src/index.ts",
+		output: {
+			file: "dist/index.browser.js",
+			format: "esm",
+			sourcemap: true,
+		},
+		plugins: [
+			tsPlugin("./tsconfig.build.json"),
+			nodeResolve({
+				browser: true,
+			}),
+			commonjs(),
+		],
+	},
+];

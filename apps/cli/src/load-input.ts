1c699100da194b902adb39c28c59c8932291f490
Guido Flohr <guido.flohr@cantanea.com>
2026-04-23 08:08:29 +0300

    change return type to Buffer

--- apps/cli/src/load-input.ts
+++ apps/cli/src/load-input.ts
@@ -1,13 +1,19 @@
 import * as fs from 'node:fs/promises';
 import { stdin } from 'node:process';
 
+/**
+ * Load the input PDF.
+ *
+ * @param filename the filename of the PDF or '-' for standard inut
+ * @returns a `Buffer` with the input Bytes.
+ */
 export async function loadInput(
 	filename: string,
-): Promise<Uint8Array<ArrayBufferLike>> {
+): Promise<Buffer> {
 	return filename === '-' ? readStdin() : fs.readFile(filename);
 }
 
-async function readStdin(): Promise<Uint8Array> {
+async function readStdin(): Promise<Buffer> {
 	const chunks: Buffer[] = [];
 
 	for await (const chunk of stdin) chunks.push(chunk);
